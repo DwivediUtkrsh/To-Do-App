f1efbd93d7c4310d42e441ec019971df318fc869
DwivediUtkrsh <utkrshdwivedi551@gmail.com>
2024-06-04 20:46:42 +0530

    Improve UI - Sidebar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,3 +1,8 @@
+import { FaBeer } from 'react-icons/fa';
+import { FaTasks } from "react-icons/fa";
+import { HiMiniInbox } from "react-icons/hi2";
+import { CiCalendar } from "react-icons/ci";
+import { FaCalendarDays } from "react-icons/fa6";
 import tasksNav from '../images/tasksNav.gif'
 import calendarNav from '../images/calendarNav.png'
 import inboxNav from '../images/inboxNav.png'
@@ -6,13 +11,13 @@ export default function Nav(){
 
     return (
         <div className=" h-[18vw] p-2 mt-10 mr-[1vw] flex gap-2">
-            <div className="icons h-[17vw] flex flex-col left-4 gap-4 items-center fixed justify-start">
-                <img src={tasksNav}    className='h-6' alt="" />
-                <img src={inboxNav}    className='h-6' alt="" />
-                <img src={upcomingNav} className='h-6' alt="" />
-                <img src={calendarNav} className='h-6' alt="" />
+            <div className="icons h-[17vw] flex flex-col left-5 gap-4 text-xl items-center fixed justify-start">
+            <FaTasks />
+            <HiMiniInbox />
+            <CiCalendar />
+            <FaCalendarDays />
             </div>
-            <div className="items h-[17vw] relative left-8 flex flex-col gap-4 items-start pl-1 justify-start">
+            <div className="items h-[17vw] relative left-8 flex flex-col gap-3 items-start pl-1 justify-start " >
                 <p className='Tasks-P'>Tasks</p>
                 <p className='Inbox-P'>Inbox</p>
                 <p className='Calendar-P'>Calendar</p>
